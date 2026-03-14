8da86ea8b090d345f81e9f2140f5750ea709ef56
Nacira <lagouilminacira@gmail.com>
2026-03-14 23:05:38 +0000

    Fix modèle - nouveaux champs

--- models/Suggestion.js
+++ models/Suggestion.js
@@ -1,50 +1,17 @@
 const mongoose = require('mongoose');
 
 const SuggestionSchema = new mongoose.Schema({
-  matricule: {
-    type: String,
-    required: true
-  },
-  nom: {
-    type: String,
-    required: true
-  },
-  departement: {
-    type: String,
-    default: ''
-  },
-  titreIdee: {
-    type: String,
-    default: ''
-  },
-  probleme: {
-    type: String,
-    required: true
-  },
-  frequence: {
-    type: String,
-    default: ''
-  },
-  zonesImpact: {
-    type: [String],
-    default: []
-  },
-  idee: {
-    type: String,
-    required: true
-  },
-  gain: {
-    type: String,
-    default: ''
-  },
-  kpisImpactes: {
-    type: [String],
-    default: []
-  },
-  dateCreation: {
-    type: Date,
-    default: Date.now
-  }
+  matricule:    { type: String, required: true },
+  nom:          { type: String, required: true },
+  departement:  { type: String, default: '' },
+  titreIdee:    { type: String, default: '' },
+  probleme:     { type: String, required: true },
+  frequence:    { type: String, default: '' },
+  zonesImpact:  { type: [String], default: [] },
+  idee:         { type: String, required: true },
+  gain:         { type: String, default: '' },
+  kpisImpactes: { type: [String], default: [] },
+  dateCreation: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Suggestion', SuggestionSchema);
